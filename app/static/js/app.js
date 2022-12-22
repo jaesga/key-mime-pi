a267253d1ac2cc25841f862abdf761a5624d7fe5
Javi Espinosa <javier.espinosagar@gmail.com>
2022-12-22 17:01:07 +0100

    Test Changes

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,9 +1,12 @@
 "use strict";
 
 const socket = io();
+let musicPlaying = false;
 let connected = false;
 let keystrokeId = 0;
 const processingQueue = [];
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
 function onSocketConnect() {
   connected = true;
@@ -88,13 +91,88 @@ function onKeyDown(evt) {
 }
 
 function onDisplayHistoryChanged(evt) {
-  if (evt.target.checked) {
-    document.getElementById('recent-keys').style.visibility = 'visible';
-  } else {
-    document.getElementById('recent-keys').style.visibility = 'hidden';
-    limitRecentKeys(0);
-  }
+  document.getElementById('recent-keys').style.visibility = 'visible';
 }
+
+
+function simulateKey(key){
+  var code = 0;
+  switch (key){
+    case 'A':
+      console.log('Key A');
+      code = 65;
+      break;
+    case 'W':
+      console.log('Key W');
+      code = 87;
+      break;
+    case 'S':
+      console.log('Key S');
+      code = 83;
+      break;
+    case 'D':
+      console.log('Key D');
+      code = 68;
+      break;
+    case ' ':
+      console.log('Key Space');
+      code = 32;
+      break;
+
+    case 'E':
+      console.log('Key E');
+      code = 69;
+      break;
+    case 'F5':
+      console.log('Key F5');
+      code = 116;
+      break;
+  }
+
+  var keyboardEvent = document.createEvent('KeyboardEvent');
+  var initMethod = typeof keyboardEvent.initKeyboardEvent !== 'undefined' ? 'initKeyboardEvent' : 'initKeyEvent';
+
+  keyboardEvent[initMethod](
+    'keydown', // event type: keydown, keyup, keypress
+    true, // bubbles
+    true, // cancelable
+    window, // view: should be window
+    false, // ctrlKey
+    false, // altKey
+    false, // shiftKey
+    false, // metaKey
+    code, // keyCode: unsigned long - the virtual key code, else 0
+    0, // charCode: unsigned long - the Unicode character associated with the depressed key, else 0
+  );
+  document.dispatchEvent(keyboardEvent);
+}
+
+const playMusic = async () => {
+  musicPlaying = true;
+
+  var notes = document.getElementById('music-notes').value;
+
+  while(musicPlaying){
+    simulateKey('E');
+
+    eval(notes);
+
+    await delay(13000);
+    simulateKey('E');
+    simulateKey('E');
+    simulateKey('E');
+    simulateKey('E');
+    simulateKey('E');
+    await delay(2000);
+    console.log("Played!");
+  }
+};
+
+function stopMusic(){
+  console.log("Stopped!");
+  musicPlaying = false;
+}
+
 
 document.querySelector('body').addEventListener("keydown", onKeyDown);
 document.getElementById('display-history-checkbox').addEventListener("change", onDisplayHistoryChanged);
