b6c40170460fdecd1a475cdae8ac385ad8dda880
Javi Espinosa <javier.espinosagar@gmail.com>
2022-12-23 15:05:18 +0100

    add logs

--- app/hid.py
+++ app/hid.py
@@ -1,6 +1,8 @@
 import time
+import logging
 
 def send(hid_path, control_keys, hid_keycode):
+    logger.info("Key Stroke")
     with open(hid_path, 'wb+') as hid_handle:
         buf = [0] * 8
         buf[0] = control_keys
@@ -11,10 +13,13 @@ def send(hid_path, control_keys, hid_keycode):
 
 
 def send_long(hid_path, control_keys, hid_keycode):
+    logger.info("Long Key Stroke")
     with open(hid_path, 'wb+') as hid_handle:
         buf = [0] * 8
         buf[0] = control_keys
         buf[2] = hid_keycode
         hid_handle.write(bytearray(buf))
+        logger.info("Key E")
         time.sleep(0.8)
+        logger.info("Key E release")
         hid_handle.write(bytearray([0] * 8))
